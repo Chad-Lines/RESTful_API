a8491ac9668c3024ce1edcfdae2e23e170c45a7a
Chad Lines <chadlines@outlook.com>
2018-09-12 16:58:25 -0600

    (W) Basic first GET api

--- rest.py
+++ rest.py
@@ -1,41 +1,14 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class TaskListAPI(Resource):
-
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('title', type=str, required=True,
-            help='No task title provided', location='json')
-        self.reqparse.add_argument('description', type=str, default="",
-            location='json')
-        super(TaskListAPI, self).__init__()
-
+class HelloWorld(Resource):
     def get(self):
-        pass
+        return { 'hello' : 'world'}
 
-    def post(self):
-        pass
+api.add_resource(HelloWorld, '/')
 
-class TaskAPI(Resource):
-    def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('title', type=str, location='json')
-        self.reqparse.add_argument('description', type=str, location='json')
-        self.reqparse.add_argument('done', type=bool, location='json')
-        super(TaskAPI, self).__init__()
-
-    def get(self, id):
-        pass
-
-    def put(self, id):
-        pass
-
-    def delete(self, id):
-        pass
-
-api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
-api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
+if __name__ == '__main__':
+    app.run(debug=True)
